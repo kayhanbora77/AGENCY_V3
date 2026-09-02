14fe31c33a24f0cd48e8ddf5fb759f6033c562b1
kayhanbora77 <kayhanboraisteps@gmail.com>
2026-09-02 11:26:17 +0300

    feat: add script to import and clean Riya India miss-connection data into DuckDB table

--- RIYA_INDIA/filter4MissConn/1_create_table.py
+++ RIYA_INDIA/filter4MissConn/1_create_table.py
@@ -4,7 +4,7 @@ import duckdb
 # =====================================================
 # CONFIG
 # =====================================================
-CSV_FILE = r"C:\Users\cagri\Desktop\RiyaIndia\Cases\Candidate_RiyaIndiaMissConnList.csv"
+CSV_FILE = r"C:\Users\cagri\Desktop\RiyaIndia\AllRiyaIndiaMissConnList\AllRiyaIndiaMissConnList.csv"
 DB_PATH = r"C:\DuckDB\my_db.duckdb"
 TABLE_NAME = "RIYAINDIA_MISSCONNECTION"
 
